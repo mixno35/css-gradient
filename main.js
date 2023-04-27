a452c5978a82b7a30727876d206fdc5f363a0a00
Alexander Mikhno <57302726+mixno35@users.noreply.github.com>
2023-04-27 04:15:00 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function goClipboard(_text, _message = 'Copied!') {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(_text);
+        return;
+    }
+    navigator.clipboard.writeText(_text).then(function() {
+        console.log("Copied!");
+        alert(_message);
+    }, function(err) {
+        console.log('Async: Could not copy text: ' + err, 'error');
+    });
+}
+
+function fallbackCopyTextToClipboard(_text, _message) {
+    var textArea = document.createElement("textarea");
+    textArea.value = _text;
+    
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log("Copied!");
+        alert(_message);
+    } catch (err) {
+        console.log('Async: Could not copy text: ' + err, 'error');
+    }
+  
+    document.body.removeChild(textArea);
+}
---
+++ make-item.js
@@ -0,0 +1,53 @@
+function makeItem(_gradient = 'background-image: linear-gradient(135deg, #FDEB71 10%, #F8D800 100%);', _position = 0) {
+    var main_container = document.createElement('li');
+        main_container.classList.add('item__css_gradient');
+        main_container.setAttribute('id', 'item__css_gradient_' + _position);
+        main_container.setAttribute('title', _gradient);
+
+    var container_gradient = document.createElement('div');
+        container_gradient.classList.add('gradinet_container');
+        container_gradient.setAttribute('style', _gradient);
+
+    var container_used_colors = document.createElement('ul');
+        container_used_colors.classList.add('used__colors');
+        container_used_colors.setAttribute('id', 'used__color_' + _position);
+
+    var container__action_menu = document.createElement('ol');
+        container__action_menu.classList.add('item__action_menu');
+
+    var item_action_copy = document.createElement('li');
+        item_action_copy.classList.add('item');
+        if (_gradient.length > 20) {
+            item_action_copy.classList.add('copy_code');
+        } else {
+            item_action_copy.classList.add('copy');
+        }
+        item_action_copy.setAttribute('title', 'Copy');
+        item_action_copy.setAttribute('onclick', 'goClipboard("' + _gradient + '")');
+
+    container__action_menu.appendChild(item_action_copy);
+
+    container_gradient.appendChild(container__action_menu);
+
+    main_container.appendChild(container_gradient);
+    main_container.appendChild(container_used_colors);
+
+    document.getElementById('container__palettes').appendChild(main_container);
+
+    appendItemUsedColors(_gradient.split(' '), 'used__color_' + _position);
+}
+
+function appendItemUsedColors(_array = '["#FFFFFF"]', _id) {
+    setTimeout(() => {
+        for (i = 0; i < _array.length; i++) {
+            if (_array[i].startsWith('#')) {
+                var strRes = _array[i].replaceAll(',', '').replaceAll(':', '').replaceAll('.', '').replaceAll(';', '');
+                var item__used_color = document.createElement('li');
+                    item__used_color.innerText = strRes;
+                    item__used_color.style.color = strRes;
+    
+                    document.getElementById(_id).appendChild(item__used_color);
+            }
+        }
+    }, 100);
+}
